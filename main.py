1782d90f7c6e591c01651cfa97f8360a9e8e62e1
kevin-chen <kc3585@nyu.edu>
2021-12-03 15:09:32 -0500

    running code, but empty text

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
 import lexile_library as lx
 import json
 
-bookshelves = ["http://www.gutenberg.org/wiki/Children%27s_Literature_(Bookshelf)"]
+bookshelves = ["http://www.gutenberg.org/wiki/Children%27s_Literature_(Bookshelf)", 
+                "http://www.gutenberg.org/wiki/Children%27s_Fiction_(Bookshelf)",
+                "http://www.gutenberg.org/wiki/Adventure_(Bookshelf)",
+                "http://www.gutenberg.org/wiki/Fantasy_(Bookshelf)",
+                "http://www.gutenberg.org/wiki/Humor_(Bookshelf)",
+                "http://www.gutenberg.org/wiki/Mystery_Fiction_(Bookshelf)",
+                "http://www.gutenberg.org/wiki/Movie_Books_(Bookshelf)",
+                "http://www.gutenberg.org/wiki/Science_Fiction_(Bookshelf)",
+                "http://www.gutenberg.org/wiki/Children%27s_History_(Bookshelf)",
+                "http://www.gutenberg.org/wiki/Plays_(Bookshelf)"]
 
 bookTitles, bookNums = lx.parseBookshelves(bookshelves)
 print("Book Titles:", bookTitles)
 
 bookMap = lx.parseBookText(bookTitles, bookNums)
-print("Book Map:", bookMap)
+# print("Book Map:", bookMap)
 
 bookTitles, bookNums, lexileScores = lx.getAllLexileScores(bookTitles, bookNums)
 print("Lexile Scores:", lexileScores)
 
-# data = lx.generateData(bookTitles, bookNums, lexileScores, bookMap)
+data = lx.generateData(bookTitles, bookNums, lexileScores, bookMap)
 # print("Output Data:", data)
-# for i in range(12):
-#     print("Grade", i, len(data[i]))
 
-# file = open("filtered-data.json", "w")
-# json.dump(data, file)
-# file.close()
+for i in range(12):
+    print("Number of Texts in Grade:", i, len(data[i]))
+
+file = open("filtered-data-12-3-2021.json", "w")
+json.dump(data, file)
+file.close()
